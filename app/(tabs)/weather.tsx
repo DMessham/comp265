fdaee5387b023b7aaf1c02a24b6b025cfb411318
DMessham <20892935+DMessham@users.noreply.github.com>
2026-01-28 16:01:44 -0600

    a little formatting tweak

--- app/(tabs)/weather.tsx
+++ app/(tabs)/weather.tsx
@@ -5,46 +5,46 @@ import { ThemedView } from "@/components/themed-view";
 import { Picker } from '@react-native-picker/picker';
 import { View, Text, StyleSheet, Button, Platform } from 'react-native';
 
-function alertMSG(temp:number) {
+function alertMSG(temp: number) {
   // some of these temps are based arbitrarily on vibe, and the use of less thans make things confusing in the positive range, but reworking it is more effort than its worth.
-  if(temp < -260) {
-    return(`THE UNIVERSE HAS BROKEN, GOOD LUCK!`)
-  }else if(temp == -69 || temp == 69){
-    return("nice")
-  }else if(temp < -70){
-    return(`🧊🧊🧊\n"It's the end of the world as we know it\n It's the end of the world as we know it\n It's the end of the world as we know it, and I feel fine"\nfrom R.E.M`)
-  }else if(temp < -45){
-    return("Congrats on the new record! Try not to freeze!")
-  }else if(temp < -32){
-    return("Cover up exposed skin to avoid frostbite, Exposed skin can freeze in less than 3 minutes.")
-  }else if(temp < -20){
-    return("Cover up exposed skin to avoid frostbite.")
-  }else if(temp < -5){
-    return("Remember to wear layers you can remove if you start to overheat, especially if you plan on being physically active.")
-  }else if(temp < 5){
-    return("Watch out for ice!\nRemember to wear layers you can remove if you start to overheat, especially if you plan on being physically active.")
-  }else if(temp < 9){
-    return("Remember to wear layers you can remove if you start to overheat, especially if you plan on being physically active.")
-  }else if(temp < 13){
-    return("It's a little chilly today, Maybe wear a light sweater.")
-  }else if(temp < 18){
-    return("It's a nice day today.")
-  }else if(temp < 23){
-    return("It's a warm day today.")
-  }else if(temp < 26){
-    return("It's a warm day today.\nSpending time outdoors? Remember to get some electrolytes with your water!")
-  }else if(temp < 33){
-    return("Remember to take breaks when outside and get some electrolytes with your water!")
-  }else if(temp < 45){
-    return("Take frequent breaks from the outdoors and remember to get some electrolytes with your water!")
-  }else if(temp == 666){
-    return("REPENT!!! THE END IS NIGH\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-  }else if(temp > 60){
-    return(`Are you on fire yet?\n"It's the end of the world as we know it\nIt's the end of the world as we know it\nIt's the end of the world as we know it, and I feel fine"\n from R.E.M`)
-  }else if(temp >= 45){
-    return("Congrats on the new record! Try not to overheat and Remember to drink some electrolytes with your water!")
-  } else{
-    return("We don't know what to say!\nSomething has broken, Please let us know about this, and include info about your location, internet connection, and device so we can fix it.")
+  if (temp < -260) {
+    return (`THE UNIVERSE HAS BROKEN, GOOD LUCK!`)
+  } else if (temp == -69 || temp == 69) {
+    return ("nice")
+  } else if (temp < -70) {
+    return (`🧊🧊🧊\n"It's the end of the world as we know it\n It's the end of the world as we know it\n It's the end of the world as we know it, and I feel fine"\nfrom R.E.M`)
+  } else if (temp < -45) {
+    return ("Congrats on the new record! Try not to freeze!")
+  } else if (temp < -32) {
+    return ("Cover up exposed skin to avoid frostbite, Exposed skin can freeze in less than 3 minutes.")
+  } else if (temp < -20) {
+    return ("Cover up exposed skin to avoid frostbite.")
+  } else if (temp < -5) {
+    return ("Remember to wear layers you can remove if you start to overheat, especially if you plan on being physically active.")
+  } else if (temp < 5) {
+    return ("Watch out for ice!\nRemember to wear layers you can remove if you start to overheat, especially if you plan on being physically active.")
+  } else if (temp < 9) {
+    return ("Remember to wear layers you can remove if you start to overheat, especially if you plan on being physically active.")
+  } else if (temp < 13) {
+    return ("It's a little chilly today, Maybe wear a light sweater.")
+  } else if (temp < 18) {
+    return ("It's a nice day today.")
+  } else if (temp < 23) {
+    return ("It's a warm day today.")
+  } else if (temp < 26) {
+    return ("It's a warm day today.\nSpending time outdoors? Remember to get some electrolytes with your water!")
+  } else if (temp < 33) {
+    return ("Remember to take breaks when outside and get some electrolytes with your water!")
+  } else if (temp < 45) {
+    return ("Take frequent breaks from the outdoors and remember to get some electrolytes with your water!")
+  } else if (temp == 666) {
+    return ("REPENT!!! THE END IS NIGH\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+  } else if (temp > 60) {
+    return (`Are you on fire yet?\n"It's the end of the world as we know it\nIt's the end of the world as we know it\nIt's the end of the world as we know it, and I feel fine"\n from R.E.M`)
+  } else if (temp >= 45) {
+    return ("Congrats on the new record! Try not to overheat and Remember to drink some electrolytes with your water!")
+  } else {
+    return ("We don't know what to say!\nSomething has broken, Please let us know about this, and include info about your location, internet connection, and device so we can fix it.")
   }
 }
 
@@ -58,8 +58,8 @@ function WeatherApp() {
     { city: 'Saskatoon', temperatureC: 22, condition: 'Sunny', feelsLikeC: 26 },
     { city: 'Regina', temperatureC: 19, condition: 'Cloudy' },
     { city: 'Prince Albert', temperatureC: -200, condition: 'Rainy' },
-    { city: 'Moose Jaw', temperatureC: 61, condition: null},
-    { city: 'HELL', temperatureC: 666, condition: 'HELP'},
+    { city: 'Moose Jaw', temperatureC: 61, condition: null },
+    { city: 'HELL', temperatureC: 666, condition: 'HELP' },
   ];
 
   const toggleUnit = () => {
@@ -78,32 +78,32 @@ function WeatherApp() {
     <ThemedView style={styles.container}>
       <ThemedText style={styles.header}>
         DMessham StormTrac
-        
+
       </ThemedText>
       <View style={styles.selectorContainer}>
-          <ThemedText style={styles.label}>
-            Select a city:
-          </ThemedText>
-          <Picker
-            style={styles.dropdown}
-            selectedValue={selectedCity}
-            onValueChange={(itemValue) => setSelectedCity(itemValue)}
-          >
+        <ThemedText style={styles.label}>
+          Select a city:
+        </ThemedText>
+        <Picker
+          style={styles.dropdown}
+          selectedValue={selectedCity}
+          onValueChange={(itemValue) => setSelectedCity(itemValue)}
+        >
           {weatherData.map((data, index) => (
             <Picker.Item key={index} value={data.city} label={data.city}>
             </Picker.Item>
           ))}
-          </Picker>
-            
-        </View>
+        </Picker>
+
+      </View>
       {selectedWeather ? (
         <ThemedView style={styles.weatherCard}>
           <ThemedText style={styles.city}>{selectedWeather.city}</ThemedText>
           <ThemedText style={styles.temperature}>
             {convertTemperature(selectedWeather.temperatureC)}°{unit}
-            <Button 
-              onPress={toggleUnit} 
-              style={styles.toggleButton} 
+            <Button
+              onPress={toggleUnit}
+              style={styles.toggleButton}
               title={`${unit === 'C' ? 'C' : 'F'}`}>
             </Button>
           </ThemedText>
@@ -114,7 +114,7 @@ function WeatherApp() {
           </ThemedText>
 
         </ThemedView>
-        
+
       ) : (
         <ThemedText style={styles.loading}>No weather data available for "{selectedCity}" right now.</ThemedText>
       )}
@@ -142,7 +142,7 @@ const styles = StyleSheet.create({
   },
   dropdown: {
     color: '#eee',
-    flex:2,
+    flex: 2,
   },
   toggleButton: {
     padding: 12,
@@ -156,7 +156,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#222',
     paddingLeft: 12,
     flexDirection: 'row',
-    margin:12,
+    margin: 12,
     ...StyleSheet.absoluteFillObject,
     top: 72,
     height: 56
@@ -182,20 +182,20 @@ const styles = StyleSheet.create({
   city: {
     fontWeight: 'bold',
     fontSize: 19,
-    lineHeight:22,
+    lineHeight: 22,
     color: '#888',
   },
   condition: {
     color: '#ccc',
     fontSize: 24,
-    lineHeight:28,
+    lineHeight: 28,
   },
   temperature: {
     fontWeight: 'bold',
     color: '#e9e9e9',
     fontSize: 36,
-    lineHeight:42,
-    marginTop:52,
+    lineHeight: 42,
+    marginTop: 52,
     flexDirection: 'row',
   },
   loading: {
